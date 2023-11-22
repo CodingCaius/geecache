19fa1cbcdcd545dcc2184fbf3719b8de978fac54
caius <3085481534@qq.com>
2023-11-22 21:27:27 +0800

    在 lru 中添加过期时间

--- lru/lru.go
+++ lru/lru.go
@@ -2,83 +2,147 @@
 
 package lru
 
-import "container/list"
+import (
+	"container/list"
+	"time"
+)
+
+type NowFunc func() time.Time
 
 // Cache is a LRU cache. It is not safe for concurrent access.
 type Cache struct {
-	maxBytes int64 // 允许使用的最大内存
-	nbytes   int64 // 当前已使用的内存
-	ll       *list.List // 双向链表
-	cache    map[string]*list.Element // 字典，键是字符串，值是双向链表中对应节点的指针
+	MaxEntries int                      // MaxEntries 是最大缓存条目数,零意味着没有限制。
+	ll         *list.List               // 双向链表
+	cache      map[any]*list.Element // 字典，键是字符串，值是双向链表中对应节点的指针
 	// optional and executed when an entry is purged.
-	OnEvicted func(key string, value Value) // 某条记录被移除时的回调函数，可以为 nil
+	OnEvicted func(key Key, value any) // 某条记录被移除时的回调函数，可以为 nil
+
+	// Now 是缓存将用来确定的 Now() 函数
+	// 用于计算过期值的当前时间
+	// 默认为 time.Now()
+	Now NowFunc
 }
+
+// A Key may be any value that is comparable. See http://golang.org/ref/spec#Comparison_operators
+type Key any
 
 // 双向链表节点的数据类型
 type entry struct {
-	key   string
-	value Value
-}
-
-// Value use Len to count how many bytes it takes
-type Value interface {
-	Len() int // 用于返回值所占用的内存大小
+	key    Key
+	value  any
+	expire time.Time
 }
 
 // New is the Constructor of Cache
-func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+// New creates a new Cache.
+// If maxEntries is zero, the cache has no limit and it's assumed
+// that eviction is done by the caller.
+func New(maxEntries int) *Cache {
 	return &Cache{
-		maxBytes:  maxBytes,
-		ll:        list.New(),
-		cache:     make(map[string]*list.Element),
-		OnEvicted: onEvicted,
-	}
-}
-
-// Get 查找键的值
-func (c *Cache) Get(key string) (value Value, ok bool) {
-	if ele, ok := c.cache[key]; ok {
-		c.ll.MoveToFront(ele) // 将链表中的节点 ele 移动到队尾
-		kv := ele.Value.(*entry)
-		return kv.value, true
-	}
-	return
-}
-
-// RemoveOldest 删除队首
-func (c *Cache) RemoveOldest() {
-	ele := c.ll.Back()
-	if ele != nil {
-		c.ll.Remove(ele)
-		kv := ele.Value.(*entry)
-		delete(c.cache, kv.key) // 从字典中 c.cache 删除该节点的映射关系
-		// 更新当前所用的内存 c.nbytes
-		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
-		// 如果回调函数 OnEvicted 不为 nil，则调用回调函数
-		if c.OnEvicted != nil {
-			c.OnEvicted(kv.key, kv.value)
-		}
+		MaxEntries: maxEntries,
+		ll:         list.New(),
+		cache:      make(map[any]*list.Element),
+		Now:        time.Now,
 	}
 }
 
 // Add 向缓存中添加一个数据
-func (c *Cache) Add(key string, value Value) {
-	if ele, ok := c.cache[key]; ok {
-		c.ll.MoveToFront(ele)
-		kv := ele.Value.(*entry)
-		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
-		kv.value = value
-	} else {
-		ele := c.ll.PushFront(&entry{key, value})
-		c.cache[key] = ele
-		c.nbytes += int64(len(key)) + int64(value.Len())
+func (c *Cache) Add(key any, value any, expire time.Time) {
+	// 初始化缓存结构体中的map和双向链表
+	if c.cache == nil {
+		c.cache = make(map[any]*list.Element)
+		c.ll = list.New()
 	}
-	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+	// 如果键已存在于缓存中，则更新其值和过期时间，并将该条目移到链表头部（表示最近访问）
+	if ee, ok := c.cache[key]; ok {
+		eee := ee.Value.(*entry)
+		if c.OnEvicted != nil {
+			c.OnEvicted(key, eee.value)
+		}
+		c.ll.MoveToFront(ee)
+		eee.expire = expire
+		eee.value = value
+	}
+
+	// 如果键不存在于缓存中，创建一个新的缓存条目并将其添加到链表头部和map中
+	ele := c.ll.PushFront(&entry{key, value, expire})
+	c.cache[key] = ele
+
+	// 如果设置了最大条目数且当前条目数超过最大值，则移除最老的条目
+	if c.MaxEntries != 0 && c.ll.Len() > c.MaxEntries {
 		c.RemoveOldest()
 	}
 }
 
-// Len the number of cache entries
+// Get 从缓存中查找键的值
+func (c *Cache) Get(key Key) (value any, ok bool) {
+	if c.cache == nil {
+		return
+	}
+	if ele, hit := c.cache[key]; hit {
+		entry := ele.Value.(*entry)
+		// 如果该条目已过期，则将其从缓存中删除
+		if !entry.expire.IsZero() && entry.expire.Before(c.Now()) {
+			c.removeElement(ele)
+			return nil, false
+		}
+
+		c.ll.MoveToFront(ele)
+		return entry.value, true
+	}
+	return
+}
+
+// Remove removes the provided key from the cache.
+func (c *Cache) Remove(key Key) {
+	if c.cache == nil {
+		return
+	}
+	if ele, hit := c.cache[key]; hit {
+		c.removeElement(ele)
+	}
+}
+
+// RemoveOldest 从缓存中删除最旧的项目。
+func (c *Cache) RemoveOldest() {
+	if c.cache == nil {
+		return
+	}
+	ele := c.ll.Back()
+	if ele != nil {
+		c.removeElement(ele)
+	}
+}
+
+func (c *Cache) removeElement(e *list.Element) {
+	// 从双向链表中移除指定的链表元素 e
+	c.ll.Remove(e)
+	// 从缓存的 map 中删除与该链表元素对应的键值对
+	kv := e.Value.(*entry)
+	delete(c.cache, kv.key)
+	// 回调函数
+	if c.OnEvicted != nil {
+		c.OnEvicted(kv.key, kv.value)
+	}
+}
+
+
+// Len returns the number of items in the cache
 func (c *Cache) Len() int {
+	if c.cache == nil {
+		return 0
+	}
 	return c.ll.Len()
+}
+
+// Clear purges all stored items from the cache.
+func (c *Cache) Clear() {
+	if c.OnEvicted != nil {
+		for _, e := range c.cache {
+			kv := e.Value.(*entry)
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+	c.ll = nil
+	c.cache = nil
 }
--- lru/lru_test.go
+++ lru/lru_test.go
@@ -1,65 +1,133 @@
 package lru
 
 import (
-	"reflect"
+	"fmt"
 	"testing"
+	"time"
 )
 
-type String string
+type simpleStruct struct {
+	int
+	string
+}
 
-func (d String) Len() int {
-	return len(d)
+type complexStruct struct {
+	int
+	simpleStruct
+}
+
+var getTests = []struct {
+	name       string
+	keyToAdd   interface{}
+	keyToGet   interface{}
+	expectedOk bool
+}{
+	{"string_hit", "myKey", "myKey", true},
+	{"string_miss", "myKey", "nonsense", false},
+	{"simple_struct_hit", simpleStruct{1, "two"}, simpleStruct{1, "two"}, true},
+	{"simple_struct_miss", simpleStruct{1, "two"}, simpleStruct{0, "noway"}, false},
+	{"complex_struct_hit", complexStruct{1, simpleStruct{2, "three"}},
+		complexStruct{1, simpleStruct{2, "three"}}, true},
+}
+
+func TestAdd_evictsOldAndReplaces(t *testing.T) {
+	var evictedKey Key
+	var evictedValue interface{}
+	lru := New(0)
+	lru.OnEvicted = func(key Key, value interface{}) {
+		evictedKey = key
+		evictedValue = value
+	}
+	lru.Add("myKey", 1234, time.Time{})
+	lru.Add("myKey", 1235, time.Time{})
+
+	newVal, ok := lru.Get("myKey")
+	if !ok {
+		t.Fatalf("%s: cache hit = %v; want %v", t.Name(), ok, !ok)
+	}
+	if newVal != 1235 {
+		t.Fatalf("%s: cache hit = %v; want %v", t.Name(), newVal, 1235)
+	}
+	if evictedKey != "myKey" {
+		t.Fatalf("%s: evictedKey = %v; want %v", t.Name(), evictedKey, "myKey")
+	}
+	if evictedValue != 1234 {
+		t.Fatalf("%s: evictedValue = %v; want %v", t.Name(), evictedValue, 1234)
+	}
 }
 
 func TestGet(t *testing.T) {
-	lru := New(int64(0), nil)
-	lru.Add("key1", String("1234"))
-	if v, ok := lru.Get("key1"); !ok || string(v.(String)) != "1234" {
-		t.Fatalf("cache hit key1=1234 failed")
-	}
-	if _, ok := lru.Get("key2"); ok {
-		t.Fatalf("cache miss key2 failed")
-	}
-}
-
-func TestRemoveoldest(t *testing.T) {
-	k1, k2, k3 := "key1", "key2", "k3"
-	v1, v2, v3 := "value1", "value2", "v3"
-	cap := len(k1 + k2 + v1 + v2)
-	lru := New(int64(cap), nil)
-	lru.Add(k1, String(v1))
-	lru.Add(k2, String(v2))
-	lru.Add(k3, String(v3))
-
-	if _, ok := lru.Get("key1"); ok || lru.Len() != 2 {
-		t.Fatalf("Removeoldest key1 failed")
+	for _, tt := range getTests {
+		lru := New(0)
+		lru.Add(tt.keyToAdd, 1234, time.Time{})
+		val, ok := lru.Get(tt.keyToGet)
+		if ok != tt.expectedOk {
+			t.Fatalf("%s: cache hit = %v; want %v", tt.name, ok, !ok)
+		} else if ok && val != 1234 {
+			t.Fatalf("%s expected get to return 1234 but got %v", tt.name, val)
+		}
 	}
 }
 
-func TestOnEvicted(t *testing.T) {
-	keys := make([]string, 0)
-	callback := func(key string, value Value) {
-		keys = append(keys, key)
+func TestRemove(t *testing.T) {
+	lru := New(0)
+	lru.Add("myKey", 1234, time.Time{})
+	if val, ok := lru.Get("myKey"); !ok {
+		t.Fatal("TestRemove returned no match")
+	} else if val != 1234 {
+		t.Fatalf("TestRemove failed.  Expected %d, got %v", 1234, val)
 	}
-	lru := New(int64(10), callback)
-	lru.Add("key1", String("123456"))
-	lru.Add("k2", String("k2"))
-	lru.Add("k3", String("k3"))
-	lru.Add("k4", String("k4"))
 
-	expect := []string{"key1", "k2"}
-
-	if !reflect.DeepEqual(expect, keys) {
-		t.Fatalf("Call OnEvicted failed, expect keys equals to %s", expect)
+	lru.Remove("myKey")
+	if _, ok := lru.Get("myKey"); ok {
+		t.Fatal("TestRemove returned a removed entry")
 	}
 }
 
-func TestAdd(t *testing.T) {
-	lru := New(int64(0), nil)
-	lru.Add("key", String("1"))
-	lru.Add("key", String("111"))
+func TestEvict(t *testing.T) {
+	evictedKeys := make([]Key, 0)
+	onEvictedFun := func(key Key, value interface{}) {
+		evictedKeys = append(evictedKeys, key)
+	}
 
-	if lru.nbytes != int64(len("key")+len("111")) {
-		t.Fatal("expected 6 but got", lru.nbytes)
+	lru := New(20)
+	lru.OnEvicted = onEvictedFun
+	for i := 0; i < 22; i++ {
+		lru.Add(fmt.Sprintf("myKey%d", i), 1234, time.Time{})
+	}
+
+	if len(evictedKeys) != 2 {
+		t.Fatalf("got %d evicted keys; want 2", len(evictedKeys))
+	}
+	if evictedKeys[0] != Key("myKey0") {
+		t.Fatalf("got %v in first evicted key; want %s", evictedKeys[0], "myKey0")
+	}
+	if evictedKeys[1] != Key("myKey1") {
+		t.Fatalf("got %v in second evicted key; want %s", evictedKeys[1], "myKey1")
+	}
+}
+
+func TestExpire(t *testing.T) {
+	var tests = []struct {
+		name       string
+		key        interface{}
+		expectedOk bool
+		expire     time.Duration
+		wait       time.Duration
+	}{
+		{"not-expired", "myKey", true, time.Second * 1, time.Duration(0)},
+		{"expired", "expiredKey", false, time.Millisecond * 100, time.Millisecond * 150},
+	}
+
+	for _, tt := range tests {
+		lru := New(0)
+		lru.Add(tt.key, 1234, time.Now().Add(tt.expire))
+		time.Sleep(tt.wait)
+		val, ok := lru.Get(tt.key)
+		if ok != tt.expectedOk {
+			t.Fatalf("%s: cache hit = %v; want %v", tt.name, ok, !ok)
+		} else if ok && val != 1234 {
+			t.Fatalf("%s expected get to return 1234 but got %v", tt.name, val)
+		}
 	}
 }
